cd1979fc7549699264a46b0ac6e0d39710977ae6
MicahelX-code <59616661+MicahelX-code@users.noreply.github.com>
2020-07-12 20:04:09 +0400

    pawn movements except en peasant and promotion

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 
-const   w = h = 512;
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-        isWhiteTurn = true;
+const   w = 512,
+        h = 512,
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
         playerSideWhite = true;
+
+let isWhiteTurn = true,
+    pgn = [],
+    selection = [-1, -1],
+    canWhiteCastleKing = true,
+    canWhiteCastleQueen = true,
+    canBlackCastleKing = true,
+    canBlackCastleQueen = true;
+
 
 function preload() {
   // load pieces
@@ -83,42 +92,68 @@ function drawPices() {
 function playerMove() {
   x = floor(mouseX * 8 / w);
   y = floor(mouseY * 8 / w);
-  if (arr[y][x][0] == 'w') {
+  if ((arr[y][x][0] == 'w' && isWhiteTurn) || (arr[y][x][0] == 'b' && !isWhiteTurn)) {
     selection = [y, x];
-    console.log(selection);
   } else {
-    if (isLegal(selection, x, y))
+    if (isLegal(selection, y, x)) {
       makeMove(selection[0], selection[1], y, x);
-    else 
-      selection = [-1, -1];
+    }
+    selection = [-1, -1];
   }
 }
 
-function isLegal(selection, x, y) {
+function isLegal(selection, y, x) {
   if (selection[0] == -1) return false;
-  // switch(arr[selection[0]][selection[1]][1]){
-  //   case 'k':
-  //     break;
-  //   case 'b':
-  //     break;
-  //   case 'r':
-  //     break;
-  //   case 'p':
-  //     break;
-  //   case 'q':
-  //     break;
-  //   case 'n':
-  //     break;
-  // }
-  return true;
+  switch(arr[selection[0]][selection[1]][1]){
+    case 'k': // king
+      return true;
+      break;
+    case 'b': // bishop
+      return true;
+      break;
+    case 'r': // rook
+      return true;
+      break;
+    // pawn
+    case 'p':
+      // regular move 1 square ahead
+      if ((arr[y][x] == '  ') && (selection[1] == x) &&
+        ((selection[0] - y == 1) && isWhiteTurn || (selection[0] - y == -1) && !isWhiteTurn)) {
+          return true;
+      // 1st move 2 squares ahead
+      } else if ((arr[y][x] == '  ') && (selection[1] == x) &&
+        ((selection[0] == 6 && isWhiteTurn) || (selection[0] == 1 && !isWhiteTurn)) &&
+        ((selection[0] - y == 2) && isWhiteTurn || (selection[0] - y == -2) && !isWhiteTurn)) {
+          return true;
+      // cupture
+      } else if ((arr[y][x][0] == 'b' && isWhiteTurn) || (arr[y][x][0] == 'w' && !isWhiteTurn)) {
+        if ((selection[0] - y == 1 && abs(selection[1] - x) == 1 && isWhiteTurn) ||
+        (selection[0] - y == -1 && abs(selection[1] - x) == 1 && !isWhiteTurn))
+          return true;
+      // en peasant
+      
+      }
+      break;
+    case 'q': // queen
+      return true;
+      break;
+    case 'n': // knight
+      return true;
+      break;
+    default:
+      return false;
+  }
 }
 
 function isInCheck() {
-
+  return false;
 }
 
 function makeMove(y0, x0, y1, x1) {
   piece = arr[y0][x0];
   arr[y0][x0] = '  ';
   arr[y1][x1] = piece;
+  isWhiteTurn = !isWhiteTurn;
+  append(pgn, [[x0, y0], [x1, y1]]);
+  console.log(pgn[pgn.length - 1]);
 }
